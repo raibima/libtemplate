05125b87891796437b75ae58491cea7454cfc698
Raibima Putra <raibima.putra@traveloka.com>
2019-07-29 22:02:12 +0700

    Tweak eslint for tests

---
+++ src/__tests__/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', 'prefer-let'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'prefer-let/prefer-let': 'error',
+  },
+};
--- src/__tests__/BasicTest.test.js
+++ src/__tests__/BasicTest.test.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import { MyComponent } from '../index';
 
 it('can render to the DOM', () => {
-  const root = document.createElement('div');
+  let root = document.createElement('div');
   ReactDOM.render(<MyComponent />, root);
   ReactDOM.unmountComponentAtNode(root);
 });
