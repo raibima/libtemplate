8ad677fba32d2fd5183dabf16190955d5965db08
Raibima Putra <raibima.putra@traveloka.com>
2019-07-29 19:30:22 +0700

    Make sure prop-types don't get bundled

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-<div />;
+function MyComponent() {
+  return <div />;
+}
 
 if (process.env.NODE_ENV === 'development') {
-  console.log('Hi!');
+  const PropTypes = require('prop-types');
+
+  MyComponent.propTypes = {
+    text: PropTypes.string,
+  };
 }
